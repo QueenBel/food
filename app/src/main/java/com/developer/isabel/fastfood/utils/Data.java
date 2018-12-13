4aac0f7247621535600d6d4de79e0a4d8fa5d796
QueenBel <bella77bel@gmail.com>
2018-12-13 10:11:26 -0400

    corrigiendo errores

--- app/src/main/java/com/developer/isabel/fastfood/utils/Data.java
+++ app/src/main/java/com/developer/isabel/fastfood/utils/Data.java
@@ -11,6 +11,9 @@ public class Data {
 
     public static String TOKEN="Data eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiY2xpZW50ZTFAZ21haWwuY29tIiwicGFzc3dvcmQiOiJjbGllbnRlMSIsImlhdCI6MTU0MzQxNjQyM30.gdJaT0YHJXWmS8JLjmf5saWdhf9JI3z6N9vNrX7NcMY";
     public static String HOST= "http://192.168.1.110:7070";
+    public static String REGISTER_CLIENT=HOST+ "/api/v1.0/clients";
+    public static String LOGIN= HOST+ "/api/v1.0/clients/login";
+    
     public static String REGISTER_RESTAURANT= HOST + "/api/v1.0/restaurant";
     public static String UPLOAD_RESTAURANT= HOST + "/api/v1.0/uploadrestaurant";
     public static String GET_RESTAURANT= HOST + "/api/v1.0/restaurant/";
